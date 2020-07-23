7bc457109ccaae9109d0c93b3977bcc6780596de
jalenjones123 <jalenjones9857@gmail.com>
2020-07-22 20:33:14 -0700

    Submission for Module 3

--- Practical_Exercise/animal_list_PE02.py
+++
@@ -1,9 +0,0 @@
-animals = ['zebra', 'dog', 'turtle', 'lion', 'cat', 'deer', 'llama']
-for animal in animals:
-    print(animal)
-
-print("The first three items in the list are: ",animals[0:3])
-
-print("Three items from the middle of the list are ",animals[2:5])
-
-print("The last three items in the list are: ",animals[4:7])
