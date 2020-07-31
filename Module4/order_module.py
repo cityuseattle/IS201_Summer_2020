7e78abe1b3c084e7a48e16a6d87e2ccaa8d3571e
Sriracha5 <64803838+Sriracha5@users.noreply.github.com>
2020-07-31 14:40:06 -0700

    module 4
    wk4 commit

---
+++ Module4/order_module.py
@@ -0,0 +1,9 @@
+def order (number, *list):
+    print(f"\nNumber of dishes: {number}, The dishes are:")
+    for list in lists:
+        lists = (1,"Pizza")
+        lists = (3,"soup","pasta","pizza")
+        print(f"- {list}")
+
+order_module(1, "pizza")
+order_module(3,"soup","pasta","pizza")
