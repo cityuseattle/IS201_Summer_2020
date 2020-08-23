febaca35abed5ac4e4558fdfde87be309e346279
Christopher Reyes <power4you@Christophers-MacBook-Air.local>
2020-08-23 00:10:19 -0700

    Submission for Module7

---
+++ IS201/ChrisReyes/Module7/Data_encapsulation.py
@@ -0,0 +1,18 @@
+class Computer:
+
+    def _init_(self):
+        self._maxprice = 900
+    def sell(self):
+        print("Selling Price: {}".format(self._maxprice))
+    def setMaxPrice(self, price):
+        self._maxprice = price
+c = Computer()
+c.sell()
+
+# change the price
+c._maxprice = 1000
+c.sell()
+
+# using setter function 
+c.setMaxPrice(1000)
+c.sell()
