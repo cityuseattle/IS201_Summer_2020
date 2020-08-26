32f64ab53b8cc712c93e939d4a768bfa9946c24a
fernando mckenzie <fernando.a.mckenzie@live.com>
2020-08-26 03:29:43 -0700

    update

--- Module4/keyword.py
+++ Module4/keyword.py
@@ -1,5 +1,5 @@
-def describe_pet(animal_type, pet_name, color="brown"):
-    print(f"\nI have a {animal_type}.")
-    print(f"my {animal_type}'s name is {pet_name.title()}. It's color is {color}.")
+def describe_pet(animal_type, color="brown"):
+    print(f"\nI have an {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}. It's color is {color}.")
 
 describe_pet(animal_type='hamster', pet_name='harry')
