ef54a791e9f2d0ac506ea639847c4d454c9271d8
Sriracha5 <64803838+Sriracha5@users.noreply.github.com>
2020-09-06 13:53:28 -0700

    Module1
    All1

---
+++ Module1/Break.py
@@ -0,0 +1,15 @@
+print("For_Break")
+for letter in 'Python':
+    if letter == 'h':
+        break
+    print ('Current Letter :', letter)
+
+print("\nwhile-Break")
+var = 10 
+while var > 0:
+    print ('current variable value:',var)
+    var = var -1
+    if var == 5:
+        break
+print("Good bye!")
+
